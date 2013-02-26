daa9eb5022e0d14d8fbc7569a36ea840f98a948f
nycz <pyntix@gmail.com>
2013-02-26 21:37:57 +0100

    Added a working status bar with pagenum and path

--- infopanel.py
+++ infopanel.py
@@ -1,19 +1,39 @@
+import os.path
 
 from PyQt4 import QtCore, QtGui, QtWebKit
 
+import common
+
+
 class InfoPanel(QtGui.QFrame):
-    def __init__(self):
-        super().__init__()
-        # <- page x/max ->    ***    <- chapter x/max ->
+    def __init__(self, parent):
+        super().__init__(parent)
+
         layout = QtGui.QGridLayout(self)
-        layout.setMargin(0)
-        layout.setSpacing(0)
+        common.kill_theming(layout)
 
         class InfoPanelLabel(QtGui.QLabel): pass
-        self.page_label = InfoPanelLabel('<- Page 1/12 ->')
-        self.chapter_label = InfoPanelLabel('<- Chapter 1/3 ->')
+        self.label = InfoPanelLabel()
+        layout.addWidget(self.label, 0, 0, QtCore.Qt.AlignHCenter)
 
-        layout.addWidget(self.page_label, 0, 0, QtCore.Qt.AlignHCenter)
-        # layout.addWidget(QtGui.QLabel('***'), 0, 1, QtCore.Qt.AlignHCenter)
-        layout.addWidget(self.chapter_label, 0, 1, QtCore.Qt.AlignHCenter)
+        self.stay_hidden = False
 
+        self.hide()
+
+    def set_info(self, page_list, index):
+        s = "<em>Page</em> {page}/{maxpages}\t–\t<strong>File</strong>: {fname}"
+        if page_list[index][1]:
+            s += "\t–\tChapter: {}".format(page_list[index][1])
+        self.label.setText(s.format(
+            page=index+1,
+            maxpages=len(page_list),
+            fname=os.path.basename(page_list[index][0]))
+        )
+
+    def show(self):
+        if not self.stay_hidden:
+            super().show()
+
+    def toggle_hidden(self):
+        self.stay_hidden = not self.stay_hidden
+        self.setHidden(self.stay_hidden)
--- viewerframe.py
+++ viewerframe.py
@@ -7,6 +7,7 @@ from PyQt4 import QtCore, QtGui, QtWebKit
 
 import common
 import datalib
+import infopanel
 
 
 class ViewerFrame(QtGui.QFrame):
@@ -46,27 +47,40 @@ class ViewerFrame(QtGui.QFrame):
         self.current_entry = []
         self.current_page = -1
 
+        # Layout
         layout = QtGui.QVBoxLayout(self)
         common.kill_theming(layout)
-        self.webview = self.WebView()
+        self.webview = self.WebView(self)
         layout.addWidget(self.webview)
+        layout.setStretchFactor(self.webview, 1)
+        self.info_panel = infopanel.InfoPanel(self)
+        layout.addWidget(self.info_panel)
+        layout.setStretchFactor(self.info_panel, 0)
 
         datalib.generate_index_page(self.root_path, self.generated_index,
                                     self.entry_pages)
         self.goto_index()
 
+        # Signals
         self.webview.next.connect(self.next)
         self.webview.previous.connect(self.previous)
+        self.webview.page().setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
+        self.webview.linkClicked.connect(self.link_clicked)
 
+        # Key shortcuts
         for key in data['hotkeys']['next']:
             common.set_hotkey(key, self, self.next)
         for key in data['hotkeys']['previous']:
             common.set_hotkey(key, self, self.previous)
         for key in data['hotkeys']['home']:
             common.set_hotkey(key, self, self.goto_index)
+        for key in data['hotkeys']['toggle_status_bar']:
+            common.set_hotkey(key, self, self.toggle_status_bar)
 
-        self.webview.page().setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
-        self.webview.linkClicked.connect(self.link_clicked)
+
+    def toggle_status_bar(self):
+        if self.current_page != -1:
+            self.info_panel.toggle_hidden()
 
     def reload(self):
         self.webview.reload()
@@ -112,7 +126,11 @@ class ViewerFrame(QtGui.QFrame):
 
     def set_page(self):
         self.webview.load(QtCore.QUrl.fromLocalFile(self.current_entry[self.current_page][0]))
+        if self.info_panel.isHidden():
+            self.info_panel.show()
+        self.info_panel.set_info(self.current_entry, self.current_page)
 
     def goto_index(self):
         self.current_page = -1
         self.webview.load(QtCore.QUrl.fromLocalFile(self.generated_index))
+        self.info_panel.hide()
