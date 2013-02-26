65eb50cfdcd2845ed6d82d5774b65536761b410c
nycz <pyntix@gmail.com>
2013-02-26 21:37:20 +0100

    Cleaned up in sapfo.py, rm'ed downloaddialog for now

--- downloaddialog.py
+++
@@ -1,1 +0,0 @@
-class DownloadDialog: pass
--- sapfo.py
+++ sapfo.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 
 import collections
-import os
-import os.path
-from os.path import join
-import re
 import sys
 
-from PyQt4 import QtCore, QtGui, QtWebKit
+from PyQt4 import QtGui
 
 import common
-from common import read_file, write_file
-import downloaddialog
-import infopanel
 from viewerframe import ViewerFrame
 
 
@@ -20,8 +13,6 @@ class MainWindow(QtGui.QFrame):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Sapfo')
-
-        download_window = downloaddialog.DownloadDialog()
 
         layout = QtGui.QVBoxLayout(self)
         common.kill_theming(layout)
@@ -46,38 +37,16 @@ class MainWindow(QtGui.QFrame):
             if name == 'default':
                 continue
             newdata = update_dict(instances['default'].copy(), data)
-            print(instances['default'].copy())
-            print(data)
-            print(newdata)
             self.tab_widget.addTab(ViewerFrame(name, newdata), name)
-
-        # vert_layout = QtGui.QVBoxLayout(self)
-        # vert_layout.setMargin(0)
-        # vert_layout.setSpacing(0)
-        # self.story_info_panel = infopanel.InfoPanel() #QtWebKit.QWebView()#
-        # self.story_info_panel.hide()
-        # vert_layout.addWidget(self.story_info_panel)
-        # vert_layout.setStretchFactor(self.story_info_panel, 0)
-
-        # self.webview = WebView()
-        # vert_layout.addWidget(self.webview)
-        # vert_layout.setStretchFactor(self.webview, 1)
 
         QtGui.QShortcut(QtGui.QKeySequence("Ctrl+R"), self, self.reload)
         QtGui.QShortcut(QtGui.QKeySequence("F5"), self, self.reload)
-        # self.webview.urlChanged.connect(self.url_changed)
 
-        self.setStyleSheet(read_file('qt.css'))
+        self.setStyleSheet(common.read_file('qt.css'))
         self.show()
 
-    # def url_changed(self, url):
-    #     if url.toString().endswith('index_page_generated.html'):
-    #         self.story_info_panel.hide()
-    #     else:
-    #         self.story_info_panel.show()
-
     def reload(self):
-        self.setStyleSheet(read_file('qt.css'))
+        self.setStyleSheet(common.read_file('qt.css'))
         self.tab_widget.currentWidget().reload()
 
 
