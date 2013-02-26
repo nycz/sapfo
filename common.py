bc278e87ae8767401261f90efb43cc0906d50fb1
nycz <pyntix@gmail.com>
2013-02-26 15:54:50 +0100

    Rewrote a buncha stuff

--- common.py
+++ common.py
@@ -5,11 +5,9 @@ def read_json(path):
     return json.loads(read_file(path))
 
 def write_json(path, data):
-    write_file(json.dumps(data, ensure_ascii=False, indent=2, sort_keys=True))
+    write_file(path, json.dumps(data, ensure_ascii=False, indent=2, sort_keys=True))
 
 def read_file(path):
-    if not os.path.isfile(path):
-        return None
     with open(path, encoding='utf-8') as f:
         data = f.read()
     return data
@@ -17,3 +15,7 @@ def read_file(path):
 def write_file(path, data):
     with open(path, 'w', encoding='utf-8') as f:
         f.write(data)
+
+def kill_theming(layout):
+    layout.setMargin(0)
+    layout.setSpacing(0)
--- sapfo.py
+++ sapfo.py
@@ -23,41 +23,65 @@ class MainWindow(QtGui.QFrame):
 
         download_window = downloaddialog.DownloadDialog()
 
-        vert_layout = QtGui.QVBoxLayout(self)
-        vert_layout.setMargin(0)
-        vert_layout.setSpacing(0)
-        self.story_info_panel = infopanel.InfoPanel() #QtWebKit.QWebView()#
-        self.story_info_panel.hide()
-        vert_layout.addWidget(self.story_info_panel)
-        vert_layout.setStretchFactor(self.story_info_panel, 0)
+        layout = QtGui.QVBoxLayout(self)
+        common.kill_theming(layout)
 
-        self.webview = WebView()
-        vert_layout.addWidget(self.webview)
-        vert_layout.setStretchFactor(self.webview, 1)
+        self.tab_widget = QtGui.QTabWidget(self)
+        layout.addWidget(self.tab_widget)
+
+        instances = common.read_json('settings.json')
+        for name, data in instances.items():
+            self.tab_widget.addTab(WebView(name, data), name)
+
+        # vert_layout = QtGui.QVBoxLayout(self)
+        # vert_layout.setMargin(0)
+        # vert_layout.setSpacing(0)
+        # self.story_info_panel = infopanel.InfoPanel() #QtWebKit.QWebView()#
+        # self.story_info_panel.hide()
+        # vert_layout.addWidget(self.story_info_panel)
+        # vert_layout.setStretchFactor(self.story_info_panel, 0)
+
+        # self.webview = WebView()
+        # vert_layout.addWidget(self.webview)
+        # vert_layout.setStretchFactor(self.webview, 1)
 
         QtGui.QShortcut(QtGui.QKeySequence("Ctrl+R"), self, self.reload)
         QtGui.QShortcut(QtGui.QKeySequence("F5"), self, self.reload)
-        self.webview.urlChanged.connect(self.url_changed)
+        # self.webview.urlChanged.connect(self.url_changed)
 
         self.setStyleSheet(read_file('qt.css'))
         self.show()
 
-    def url_changed(self, url):
-        if url.toString().endswith('index_page_generated.html'):
-            self.story_info_panel.hide()
-        else:
-            self.story_info_panel.show()
+    # def url_changed(self, url):
+    #     if url.toString().endswith('index_page_generated.html'):
+    #         self.story_info_panel.hide()
+    #     else:
+    #         self.story_info_panel.show()
 
     def reload(self):
         self.setStyleSheet(read_file('qt.css'))
-        self.webview.reload()
+        self.tab_widget.currentWidget().reload()
+
 
 class WebView(QtWebKit.QWebView):
-    def __init__(self):
+    def __init__(self, title, data):
         super().__init__()
+        self.title = title
+        self.root_path = os.path.normpath(data['path'])
+        self.generated_index = join(self.root_path,
+                    'index_page_generated_{}.html'.format(title))
+        self.fname_rx = re.compile(data['name_filter'], re.IGNORECASE)
+        self.entry_pages = {d:generate_page_links(join(self.root_path, d),
+                                                self.fname_rx,
+                                                data['blacklist'])\
+                            for d in os.listdir(self.root_path)
+                            if os.path.isdir(join(self.root_path,d))}
+        # print(self.entry_pages)
+        self.current_entry = []
+        self.current_page = -1
 
-        generate_index_page()
-        self.setUrl(QtCore.QUrl('index_page_generated.html'))
+        generate_index_page(self.root_path, self.generated_index, self.entry_pages)
+        self.goto_index()
 
         self.page().setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
         self.linkClicked.connect(self.link_clicked)
@@ -71,60 +95,119 @@ class WebView(QtWebKit.QWebView):
             ext = os.path.splitext(rawurl)[1]
             if ext == '.json':
                 os.startfile(os.path.normpath(rawurl))
-            elif ext == '.html':
-                self.setUrl(url)
+            elif os.path.basename(rawurl) == 'start_here.sapfo':
+                self.start_entry(os.path.dirname(rawurl))
+
+    def start_entry(self, path):
+        self.current_entry = self.entry_pages[os.path.basename(path)]
+        self.current_page = 0
+        self.set_page()
+
+    def next(self):
+        if not self.current_entry:
+            return
+        if self.current_page < len(self.current_entry)-1:
+            self.current_page += 1
+            self.set_page()
+        elif self.current_page == len(self.current_entry)-1:
+            self.goto_index()
+
+    def previous(self):
+        if not self.current_entry:
+            return
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.set_page()
+        elif self.current_page == 0:
+            self.goto_index()
+        elif self.current_page == -1:
+            self.current_page = len(self.current_entry)-1
+            self.set_page()
+
+    def set_page(self):
+        self.setUrl(QtCore.QUrl.fromLocalFile(self.current_entry[self.current_page][0]))
+
+    def goto_index(self):
+        self.current_page = -1
+        self.setUrl(QtCore.QUrl.fromLocalFile(self.generated_index))
 
     # Override
     def mouseReleaseEvent(self, ev):
         if ev.button() == QtCore.Qt.XButton1:
-            self.back()
+            self.previous()
         elif ev.button() == QtCore.Qt.XButton2:
-            self.forward()
+            self.next()
         else:
             super().mouseReleaseEvent(ev)
 
     # Override
     def reload(self):
-        # ugly ugly UGLY hack
-        if self.url().toString().endswith('index_page_generated.html'):
-            generate_index_page()
+        if self.url() == QtCore.QUrl.fromLocalFile(self.generated_index):
+            generate_index_page(self.root_path, self.generated_index, self.entry_pages)
         super().reload()
 
-def generate_index_page():
-    root_path = os.path.normpath(common.read_json('settings.json')['root_path'])
 
-    ignorefiles = ['literoticadownloader.py', 'literoticatemplate.html']
-    entries = [generate_entry(root_path, x) for x in os.listdir(root_path)
-               if x not in ignorefiles and os.path.isdir(join(root_path,x))]
+def generate_index_page(root_path, generated_index, entry_page_list):
+    entries = [get_entry_data(root_path, x, entry_page_list)\
+               for x in os.listdir(root_path)
+               if os.path.isdir(join(root_path,x))]
 
     html_template = read_file('index_page_template.html')
-    css = read_file('index_page.css')
-
-    write_file('index_page_generated.html',
-            html_template.format(body='\n<hr />\n'.join(entries), css=css))
-
-
-def generate_entry(root_path, path):
     entry_template = read_file('entry_template.html')
 
-    metadata = common.read_json(join(root_path, path, path + '.json'))
+    formatted_entries = [format_entry(entry_template, **data) \
+            for data in sorted(entries, key=lambda x:x['title'])]
 
-    if metadata['description']:
-        desc = metadata['description']
-    else:
-        desc = '<span class="empty_desc">[no desc]</span>'
+    write_file(generated_index, html_template.format(\
+                        body='\n<hr />\n'.join(formatted_entries),
+                        css=read_file('index_page.css')))
 
-    page_count = generate_page_count(join(root_path, path))
 
-    first_page_link = join(root_path, path ,
-            get_first_page_relative_url(root_path, path)).replace('\\', '/')
-    first_page_link = QtCore.QUrl.fromLocalFile(first_page_link).toString()
-    tags = ['<span class="tag" style="background-color:{color};">{tag}</span>'.format(color=tag_color(x),tag=x)\
-            for x in sorted(metadata['tags'])]
-    edit_url = QtCore.QUrl.fromLocalFile(join(root_path, path, path + '.json'))
+def get_entry_data(root_path, path, entry_page_list):
+    """
+    Return a dict with all relevant data from an entry.
 
-    return entry_template.format(link=first_page_link, title=path, desc=desc,
-             tags=''.join(tags), edit=edit_url.toString(), page_count=page_count)
+    No formatting or theming!
+    """
+    metadata_path = join(root_path, path, 'metadata.json')
+    try:
+        metadata = common.read_json(metadata_path)
+    except:
+        metadata = {
+            "description": "",
+            "tags": [],
+            "title": path
+        }
+        common.write_json(metadata_path, metadata)
+    start_link = join(root_path, path, 'start_here.sapfo')
+    return {
+        'title': metadata['title'],
+        'desc': metadata['description'],
+        'tags': metadata['tags'],
+        'edit_url': QtCore.QUrl.fromLocalFile(metadata_path).toString(),
+        'start_link': QtCore.QUrl.fromLocalFile(start_link).toString(),
+        'page_count': len(entry_page_list[path])
+    }
+
+def format_entry(template, title, desc, tags, edit_url, start_link, page_count):
+    """
+    Return the formatted entry as a html string.
+    """
+    desc = desc if desc else '<span class="empty_desc">[no desc]</span>'
+
+    tag_template = '<span class="tag" style="background-color:{color};">{tag}</span>'
+    tags = [tag_template.format(color=tag_color(x),tag=x)\
+            for x in sorted(tags)]
+
+    return template.format(
+        link=start_link,
+        title=title,
+        desc=desc,
+        tags=''.join(tags),
+        edit=edit_url,
+        page_count='({} pages)'.format(page_count) if page_count != 1 else ''
+    )
+
 
 
 def tag_color(text):
@@ -140,53 +223,34 @@ def tag_color(text):
     # return 'hsl({h}, {s:.0%}, {l:.0%})'.format(h=h, s=s, l=l)
     return 'rgb({r}, {g}, {b})'.format(r=r, g=g, b=b)
 
-def generate_page_count(path):
-    dir_rx = re.compile(r'Ch\. \d\d$')
 
-    items = os.listdir(path)
-    files = [x for x in items
-             if os.path.isfile(join(path, x)) \
-             and os.path.splitext(x)[1] == '.html']
+def generate_page_links(path, name_rx, blacklist):
+    """
+    Return a list of all pages in the directory.
 
-    page_count = len(files)
+    Files in the root directory should always be loaded first, then files in
+    the subdirectories.
+    """
+    # name_rx = re.compile(name_filter)
+    files = []
+    if '.' not in blacklist:
+        files = [(f, '') for f in sorted(os.listdir(path))
+                 if os.path.isfile(join(path, f))\
+                 and name_rx.search(f) and f not in blacklist]
 
-    dirs = [x for x in items
-            if os.path.isdir(join(path, x)) \
-            and dir_rx.match(x)]
+    dirs = [d for d in os.listdir(path)
+            if os.path.isdir(join(path, d))\
+            and d not in blacklist]
 
-    for d in dirs:
-        sub_files = [x for x in os.listdir(join(path, d))
-                     if os.path.isfile(join(path, d, x)) \
-                     and os.path.splitext(x)[1] == '.html']
-        page_count += len(sub_files)
-
-    if page_count == 1:
-        return ""
-
-    if not dirs:
-        return '({} pages)'.format(page_count)
-
-    return '({} chapter{}, {} pages)'.format(len(dirs), 's'*(len(dirs)>1), page_count)
+    for d in sorted(dirs):
+        files.extend([(join(f,d),d) for f in sorted(os.listdir(join(path, d)))
+                      if os.path.isfile(join(path, d, f))\
+                      and name_rx.match(f)\
+                      and d + '/' + f not in blacklist])
+    # print(len(files))
+    return [(join(path, f), subdir) for f,subdir in files]
 
 
-def get_first_page_relative_url(root_path, path):
-    root_files = os.listdir(join(root_path, path))
-    ext = '.html'
-    pagenum = ' - Page 1'
-    chapter = ' Ch. 01'
-    if path + ext in root_files:
-        return path + ext
-    elif path + pagenum + ext in root_files:
-        return path + pagenum + ext
-    elif path + chapter + ext in root_files:
-        return path + chapter + ext
-    elif chapter.strip() in root_files and os.path.isdir(join(root_path, path, chapter.strip())):
-        sub_files = os.listdir(join(root_path, path, chapter.strip()))
-        if path + chapter + ext in sub_files:
-            return join(chapter.strip(), path + chapter + ext)
-        elif path + chapter + pagenum + ext in sub_files:
-            return join(chapter.strip(), path + chapter + pagenum + ext)
-    return '#'
 
 
 def main():
