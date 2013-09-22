7c2cd470777282a1d14b5eed442d3a3805b798ae
nycz <pyntix@gmail.com>
2013-09-22 14:24:47 +0200

    Added hotkey to see filename (F6) when viewing an entry
    No path though, just the name. Useful for blacklisting.

--- infopanel.py
+++ infopanel.py
@@ -1,6 +1,8 @@
+from os.path import basename
+
 from PyQt4 import QtCore, QtGui
 
-from libsyntyche.common import kill_theming
+from libsyntyche.common import kill_theming, set_hotkey
 
 
 class InfoPanel(QtGui.QFrame):
@@ -11,12 +13,21 @@ class InfoPanel(QtGui.QFrame):
         kill_theming(layout)
 
         class InfoPanelLabel(QtGui.QLabel): pass
+        self.fname_label = InfoPanelLabel()
+        self.fname_label.setHidden(True)
+        layout.addWidget(self.fname_label, 0, 0, QtCore.Qt.AlignHCenter)
         self.label = InfoPanelLabel()
-        layout.addWidget(self.label, 0, 0, QtCore.Qt.AlignHCenter)
+        layout.addWidget(self.label, 1, 0, QtCore.Qt.AlignHCenter)
+
+        set_hotkey("F6", self, self.toggle_filename)
 
         self.data = {}
 
         self.show()
+
+    def toggle_filename(self):
+        self.fname_label.setHidden(self.fname_label.isVisible())
+
 
     def set_data(self, data=None, pagenr=0):
         if data is not None:
@@ -31,6 +42,7 @@ class InfoPanel(QtGui.QFrame):
             fname=self.data['title'],
             wordcount=wc
         ))
+        self.fname_label.setText(basename(self.data['pages'][pagenr]))
 
     def set_fullscreen(self, fullscreen):
         self.setHidden(fullscreen)
