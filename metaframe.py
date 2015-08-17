1d59cea44e5aad9525cd6b1b1cc597d6e6a2da1e
nycz <pyntix@gmail.com>
2015-08-17 20:20:01 +0200

    Refactored meta viewer, moved stuff to custom TabBar class
    Made the commands a bit clearer.

--- metaframe.py
+++ metaframe.py
@@ -44,6 +44,106 @@ class Formatter(QtGui.QSyntaxHighlighter):
                 self.setFormat(chunk.start(), chunk.end()-chunk.start(), format)
 
 
+class TabBar(QtGui.QTabBar):
+    def __init__(self, parent, print_):
+        super().__init__(parent)
+        self.print_ = print_
+        self.pages = []
+
+    def change_tab(self, direction):
+        currenttab = self.currentIndex()
+        if direction > 0 and currenttab == self.count() - 1:
+            newtab = 0
+        elif direction < 0 and currenttab == 0:
+            newtab = self.count() - 1
+        else:
+            newtab = currenttab + direction
+        self.setCurrentIndex(newtab)
+
+    def clear(self):
+        while self.count() > 0:
+            self.removeTab(0)
+
+    def current_page_fname(self):
+        i = self.currentIndex()
+        return self.pages[i][1]
+
+    def get_page_fname(self, i):
+        return self.pages[i][1]
+
+    def load_pages(self, root):
+        """
+        Read all pages from the specified directory and build a list of them.
+        """
+        fnames = os.listdir(root)
+        for f in fnames:
+            firstline, data = read_file(join(root, f)).split('\n', 1)
+            try:
+                jsondata = json.loads(firstline)
+            except ValueError:
+                self.print_('Bad/no properties found on page {}, fixing...'.format(f))
+                jsondata = json.dumps({'title': f, 'created': datetime.now().isoformat()})
+                write_file(join(root, f), '\n'.join(jsondata, firstline, data))
+                yield [f, f]
+            else:
+                yield [jsondata['title'], f]
+
+    def open_entry(self, root):
+        """
+        Ready the tab bar for a new entry.
+        """
+        self.pages = []
+        self.clear()
+        fnames = os.listdir(root)
+        self.pages = sorted(self.load_pages(root))
+        for title, _ in self.pages:
+            self.addTab(title)
+
+    def add_page(self, fname):
+        """
+        Add a new page to and then sort the tab bar. Return the index of the
+        new tab.
+
+        Raise KeyError if the title already exists.
+        """
+        if fname in (title for title, fname in self.pages):
+            raise KeyError('Page name already exists')
+        self.pages.append([fname, fname])
+        self.pages.sort()
+        i = next(zip(*self.pages)).index(fname)
+        self.insertTab(i, fname)
+        # self.setCurrentIndex(i)
+        return i
+
+    def remove_page(self):
+        """
+        Remove the active page from the tab bar and return the page's file name.
+        Note that the actual file on the disk is not removed by this.
+
+        Raise IndexError if there is only one tab left.
+        """
+        if self.count() <= 1:
+            raise IndexError('Can\'t remove the only page')
+        i = self.currentIndex()
+        page = self.pages.pop(i)
+        self.removeTab(i)
+        self.print_('Page "{}" deleted'.format(page[0]))
+        return page[1]
+
+    def rename_page(self, newtitle):
+        """
+        Rename the active page and update the tab bar.
+
+        Raise KeyError if the title already exists.
+        """
+        if newtitle in (title for title, fname in self.pages):
+            raise KeyError('Page name already exists')
+        i = self.currentIndex()
+        self.pages[i][0] = newtitle
+        self.pages.sort()
+        self.setTabText(i, newtitle)
+        self.moveTab(i, next(zip(*self.pages)).index(newtitle))
+
 
 class MetaFrame(QtGui.QFrame):
     show_index = pyqtSignal()
@@ -51,29 +151,30 @@ class MetaFrame(QtGui.QFrame):
 
     def __init__(self, parent):
         super().__init__(parent)
-        self.setDisabled(True)
-        class MetaTitle(QtGui.QLabel):
-            pass
-        self.titlelabel = MetaTitle()
-        self.tabbar = QtGui.QTabBar()
-        self.tabbar.currentChanged.connect(self.tab_changed)
-        class MetaTabCounter(QtGui.QLabel):
-            pass
-        self.tabcounter = MetaTabCounter(self)
+
         class MetaTextEdit(QtGui.QTextEdit):
             pass
         self.textarea = MetaTextEdit()
         self.textarea.setTabStopWidth(30)
         self.textarea.setAcceptRichText(False)
+        class MetaTitle(QtGui.QLabel):
+            pass
+        self.titlelabel = MetaTitle()
+        class MetaTabCounter(QtGui.QLabel):
+            pass
+        self.tabcounter = MetaTabCounter(self)
         self.terminal = MetaTerminal(self)
+        self.tabbar = TabBar(self, self.terminal.print_)
+        self.tabbar.currentChanged.connect(self.tab_changed)
+
         self.create_layout(self.titlelabel, self.tabbar, self.tabcounter,
                            self.textarea, self.terminal)
         self.connect_signals()
 
         self.formatter = Formatter(self.textarea)
 
-        set_hotkey('Ctrl+PgUp', self, lambda: self.change_tab(-1))
-        set_hotkey('Ctrl+PgDown', self, lambda: self.change_tab(+1))
+        set_hotkey('Ctrl+PgUp', self, lambda: self.tabbar.change_tab(-1))
+        set_hotkey('Ctrl+PgDown', self, lambda: self.tabbar.change_tab(+1))
         set_hotkey('Ctrl+S', self, self.save_page)
         set_hotkey('Escape', self, self.toggle_terminal)
 
@@ -126,117 +227,99 @@ class MetaFrame(QtGui.QFrame):
             self.terminal.hide()
             self.textarea.setFocus()
 
-    def change_tab(self, direction):
-        currenttab = self.tabbar.currentIndex()
-        if direction > 0 and currenttab == self.tabbar.count() - 1:
-            newtab = 0
-        elif direction < 0 and currenttab == 0:
-            newtab = self.tabbar.count() - 1
-        else:
-            newtab = currenttab + direction
-        self.tabbar.setCurrentIndex(newtab)
-
-    def set_entry(self, entry):
-        while self.tabbar.count() > 0:
-            self.tabbar.removeTab(0)
-        self.root = entry.file + '.metadir'
-        self.make_sure_metadir_exists(self.root)
-        self.files = sorted(os.listdir(self.root))
-        for f in self.files:
-            firstline, data = read_file(join(self.root, f)).split('\n', 1)
-            try:
-                jsondata = json.loads(firstline)
-            except ValueError:
-                self.terminal.print_('Bad/no properties found on page {}, fixing...'.format(f))
-                jsondata = json.dumps({'title': f, 'created': datetime.now().isoformat()})
-                write_file(join(self.root, f), jsondata + '\n' + firstline + '\n' + data)
-                title = f
-            else:
-                title = jsondata['title']
-            self.tabbar.addTab(title)
-        self.update_tabcounter()
-        self.titlelabel.setText(entry.title)
-        self.setEnabled(True)
-        self.textarea.setFocus()
-
     def update_tabcounter(self):
         self.tabcounter.setText('{}/{}'.format(self.tabbar.currentIndex()+1, self.tabbar.count()))
 
     def tab_changed(self, tabnum):
-        self.tabbar.setCurrentIndex(tabnum)
+        """
+        This is called every time the tab is changed, either "automatically"
+        (eg. by mousewheel over the tab bar) or programmatically whenever the
+        next/prev tab hotkeys are pressed.
+        """
         self.update_tabcounter()
-        firstline, data = read_file(join(self.root, self.files[tabnum])).split('\n', 1)
+        fname = self.current_page_path()
+        firstline, data = read_file(fname).split('\n', 1)
         self.textarea.setPlainText(data)
 
+    def set_entry(self, entry):
+        """
+        Load an entry, filling the tab bar with all pages etc.
+
+        This is the first thing that's called whenever the metaviewer is booted up.
+        """
+        self.root = entry.file + '.metadir'
+        self.make_sure_metadir_exists(self.root)
+        self.tabbar.open_entry(self.root)
+        self.update_tabcounter()
+        self.titlelabel.setText(entry.title)
+        self.textarea.setFocus()
+
     def make_sure_metadir_exists(self, root):
+        """
+        Create a directory with a stub page if none exist.
+        """
         if not os.path.exists(root):
             os.mkdir(root)
             data = json.dumps({'title': 'about.txt', 'created': datetime.now().isoformat()})
             write_file(join(root, 'about.txt'), data + '\n')
 
+    def current_page_path(self):
+        """ Return the current page's full path, including root dir """
+        return join(self.root, self.tabbar.current_page_fname())
+
+    # ======= COMMANDS ========================================================
+
     def new_page(self, fname):
         f = join(self.root, fname)
         if os.path.exists(f):
-            self.terminal.error('File already exists: "{}"'.format(fname))
+            self.terminal.error('File already exists')
             return
-        write_file(f, json.dumps({'title': fname, 'created': datetime.now().isoformat()}) + '\n')
-        self.files = sorted(self.files + [fname])
-        i = self.files.index(fname)
-        self.tabbar.insertTab(i, fname)
-        self.tabbar.setCurrentIndex(i)
+        try:
+            newtab = self.tabbar.add_page(fname)
+        except KeyError as e:
+            self.terminal.error(e.args[0])
+        else:
+            write_file(f, json.dumps({'title': fname, 'created': datetime.now().isoformat()}) + '\n')
+            # Do this afterwards to have something to load into textarea
+            self.tabbar.setCurrentIndex(newtab)
 
     def delete_page(self, arg):
-        if self.tabbar.count() <= 1:
-            self.terminal.error('Can\'t remove the only page')
-            return
         if arg != '!':
             self.terminal.error('Use d! to confirm deletion')
             return
-        tabnum = self.tabbar.currentIndex()
-        page = self.tabbar.tabText(tabnum)
-        os.remove(join(self.root, self.files[tabnum]))
-        self.files.pop(tabnum)
-        self.tabbar.removeTab(tabnum)
-        self.terminal.print_('Page "{}" deleted'.format(page))
+        try:
+            fname = self.tabbar.remove_page()
+        except IndexError as e:
+            self.terminal.error(e.args[0])
+        else:
+            os.remove(join(self.root, fname))
 
     def rename_page(self, title):
-        if title in (self.tabbar.tabText(n) for n in range(self.tabbar.count())):
-            self.terminal.error('Page name already exists')
-            return
-        tabnum = self.tabbar.currentIndex()
-        firstline, data = read_file(join(self.root, self.files[tabnum])).split('\n', 1)
-        jsondata = json.loads(firstline)
-        jsondata['title'] = title
-        self.tabbar.setTabText(tabnum, title)
-        write_file(join(self.root, self.files[tabnum]), json.dumps(jsondata) + '\n' + data)
+        try:
+            self.tabbar.rename_page(title)
+        except KeyError as e:
+            self.terminal.error(e.args[0])
+        else:
+            fname = self.current_page_path()
+            firstline, data = read_file(fname).split('\n', 1)
+            jsondata = json.loads(firstline)
+            jsondata['title'] = title
+            write_file(fname, json.dumps(jsondata) + '\n' + data)
 
     def save_page(self, _):
-        tabnum = self.tabbar.currentIndex()
-        firstline, _ = read_file(join(self.root, self.files[tabnum])).split('\n', 1)
+        fname = self.current_page_path()
+        firstline, _ = read_file(fname).split('\n', 1)
         data = self.textarea.toPlainText()
-        write_file(join(self.root, self.files[tabnum]), firstline + '\n' + data)
+        write_file(fname, firstline + '\n' + data)
 
     def print_filename(self, arg):
-        tabnum = self.tabbar.currentIndex()
+        fname = self.current_page_path()
         if arg == 'c':
-            firstline, _ = read_file(join(self.root, self.files[tabnum])).split('\n', 1)
+            firstline, _ = read_file(fname).split('\n', 1)
             date = json.loads(firstline)['created']
             self.terminal.print_('File created at ' + date)
         else:
-            self.terminal.print_(self.files[tabnum])
-
-# Testing formatting yo
-
-    # def set_header(self, header):
-    #     print('header',header)
-    #     cursor = self.textarea.textCursor()
-    #     format = QtGui.QTextCharFormat()
-    #     format.setFontPointSize(30)
-    #     if not cursor.hasSelection():
-    #         cursor.select(QtGui.QTextCursor.BlockUnderCursor)
-    #     cursor.mergeCharFormat(format)
-    #     self.textarea.mergeCurrentCharFormat(format)
-    #     self.textarea.setPlainText(self.textarea.toHtml())
+            self.terminal.print_(self.tabbar.current_page_fname())
 
 
 
