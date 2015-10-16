7257e89fe93859da0887c43715101a83c2d28da1
nycz <pyntix@gmail.com>
2015-10-16 13:08:25 +0200

    Removed filename status bar from infopanel
    I kept forgetting it even existed soooooo

--- infopanel.py
+++ infopanel.py
@@ -8,24 +8,12 @@ from libsyntyche.common import kill_theming, set_hotkey
 class InfoPanel(QtGui.QFrame):
     def __init__(self, parent):
         super().__init__(parent)
-
         layout = QtGui.QGridLayout(self)
         kill_theming(layout)
-
         class InfoPanelLabel(QtGui.QLabel): pass
-        self.fname_label = InfoPanelLabel()
-        self.fname_label.setHidden(True)
-        layout.addWidget(self.fname_label, 0, 0, QtCore.Qt.AlignHCenter)
         self.label = InfoPanelLabel()
         layout.addWidget(self.label, 1, 0, QtCore.Qt.AlignHCenter)
-
-        set_hotkey("F6", self, self.toggle_filename)
-
         self.show()
-
-    def toggle_filename(self):
-        self.fname_label.setHidden(self.fname_label.isVisible())
-
 
     def set_data(self, data):
         wc = "<em>({:,})</em>".format(data.wordcount)
@@ -34,7 +22,6 @@ class InfoPanel(QtGui.QFrame):
             fname=data.title,
             wordcount=wc
         ))
-        self.fname_label.setText(basename(data.file))
 
     def set_fullscreen(self, fullscreen):
         self.setHidden(fullscreen)
